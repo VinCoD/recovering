314203b10b5f662175a46c6eb1cdebc689b69baf
VinCoD <odipovincent161@gmail.com>
2021-11-08 20:33:45 +0300

    Made some modifications on the page frontend.

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = "testapp"
+
 urlpatterns = [
-    path('home', views.index, name='index'),
-    path('home/<int:index_id>/', views.about, name='about')
+    path('', views.index, name='index'),
+    path('abouts/', views.abouts, name='abouts'),
+    path('abouts/<int:index_id>/', views.about, name='about'),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -3,13 +3,20 @@ from .models import Test
 
 # Create your views here.
 
-def index(request):
+def abouts(request):
     tests = Test.objects.all()
     context = {'tests':tests}
-    return render(request, 'testapp/index.html', context)
+    return render(request, 'testapp/abouts.html', context)
+
+def index(request):
+    return render(request, 'testapp/base.html')
+
+
 
 def about(request, index_id):
     tests = Test.objects.get(id = index_id)
     news = tests.new_set.order_by('-time_added')
     context = {"news": news, "tests":tests}
     return render(request, 'testapp/about.html', context)
+
+
