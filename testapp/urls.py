2a3f73bd4d954d257be86a3140c5d2d9cb86d38b
VinCoD <odipovincent161@gmail.com>
2021-11-15 08:28:48 +0300

    Users can now add their own data.

--- testapp/urls.py
+++ testapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('abouts/', views.abouts, name='abouts'),
     path('abouts/<int:about_id>/', views.about, name='about'),
+    path('new_test/', views.new_test, name="new_test")
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Test
+from .forms import TestForm
 
 # Create your views here.
 
 def abouts(request):
-    tests = Test.objects.all()
-    context = {'tests':tests}
+    abouts = Test.objects.all()
+    context = {'abouts':abouts}
     return render(request, 'testapp/abouts.html', context)
 
 def index(request):
@@ -19,5 +20,17 @@ def about(request, about_id):
     context = {"about":about, "news": news}
 
     return render(request, 'testapp/about.html', context)
+
+def new_test(request):
+    if request.method != 'POST':
+        form = TestForm()
+    else:
+        form = TestForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('testapp:abouts')
+    context = {'form': form}
+    
+    return render(request, 'testapp/new_test.html', context)
 
 
